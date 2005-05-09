986226a7d2bc87d4a69a670fe2016ab0ca9c0484
harbourn <harbourn@9a5663bd-6d3a-0410-9de0-8d6a123c63df>
2005-05-09 23:56:19 +0000

    Initial import.

---
+++ branches/fsf/dcfldd/hash.h
@@ -0,0 +1,104 @@
+/* $Id$
+ * dcfldd - The Enhanced Forensic DD
+ * By Nicholas Harbour
+ */
+
+/* Copyright (C) 85, 90, 91, 1995-2001, 2005 Free Software Foundation, Inc.
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* GNU dd originally written by Paul Rubin, David MacKenzie, and Stuart Kemp. */
+
+#ifndef HASH_H
+#define HASH_H
+
+#include "dcfldd.h"
+#include "md5.h"
+#include "sha1.h"
+#include "sha2.h"
+
+#include <sys/types.h>
+#include <stdio.h>
+
+/* bytes_in_window and bytes_in_total are only used for dd_copy() */
+extern off_t bytes_in_window;  
+extern off_t bytes_in_total;
+
+extern void (*hashinit)(void *);
+extern void (*hashupdate)(void *, const void *, size_t);
+extern void (*hashfinal)(void *, void *);
+
+extern void *hashstr_buf;
+extern size_t hashstr_buf_size;
+
+typedef uint32_t hashflag_t;
+
+extern hashflag_t hashflags;
+
+typedef struct hashtype
+{
+    char *name;
+    hashflag_t flag;
+    void *window_context;
+    void *total_context;
+    void *vwindow_context;
+    void *vtotal_context;
+    void (*init)(void *);
+    void (*update)(void *, const void *, size_t);
+    void (*final)(void *, void *);
+    void *hashstr_buf;
+    size_t hashstr_buf_size;
+    FILE *log;
+} hashtype_t;
+
+typedef struct hashlist_s
+{
+    struct hashlist_s *next;
+    hashtype_t *hash;
+} hashlist_t;
+
+extern hashlist_t *ihashlist;
+
+extern struct hashtype hashops[];
+
+extern FILE *hash_log;
+
+/* this enum order must correspond to their position in the hashops[] array */
+enum {MD5 = 0, SHA1, SHA256, SHA384, SHA512};
+
+enum {WINDOW_CTX, TOTAL_CTX, VWINDOW_CTX, VTOTAL_CTX};
+
+#ifndef VERIFY_HASH
+#define VERIFY_HASH MD5
+#endif
+
+extern off_t bytes_in_window;
+extern off_t bytes_in_total;
+extern off_t hash_windowlen;
+extern off_t window_beginning;
+
+extern void display_windowhash(hashlist_t *, off_t);
+extern void display_totalhash(hashlist_t *, int);
+extern void display_hashalgorithm(hashlist_t *);
+extern void hash_update(hashlist_t *, void *, size_t);
+extern void hash_update_buf(hashlist_t *, int, int, void *, size_t);
+extern void hash_remainder(hashlist_t *, int);
+
+/* inner hashl_* funcitons are for iterating over hashlists */
+extern void hashl_init(hashlist_t *, int);
+extern void hashl_update(hashlist_t *, int, const void *, size_t);
+extern void hashl_final(hashlist_t *, int);
+
+#endif /* HASH_H */
---
+++ branches/fsf/dcfldd/util.c
@@ -0,0 +1,146 @@
+/* $Id$
+ * dcfldd - The Enhanced Forensic DD
+ * By Nicholas Harbour
+ */
+
+/* Copyright (C) 85, 90, 91, 1995-2001, 2005 Free Software Foundation, Inc.
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+
+/* GNU dd originally written by Paul Rubin, David MacKenzie, and Stuart Kemp. */
+
+/* Return nonzero iff the file referenced by FDESC is of a type for
+   which lseek's return value is known to be invalid on some systems.
+   Otherwise, return zero.
+   For example, return nonzero if FDESC references a character device
+   (on any system) because the lseek on many Linux systems incorrectly
+   returns an offset implying it succeeds for tape devices, even though
+   the function fails to perform the requested operation.  In that case,
+   lseek should return nonzero and set errno.  */
+
+#include "dcfldd.h"
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int buggy_lseek_support(int fdesc)
+{
+/* We have to resort to this because on some systems, lseek doesn't work
+   on some special files but doesn't return an error, either.
+   In particular, the Linux tape drivers are a problem.
+   For example, when I did the following using dd-4.0y or earlier on a
+   Linux-2.2.17 system with an Exabyte SCSI tape drive:
+   
+   dev=/dev/nst0
+   reset='mt -f $dev rewind; mt -f $dev fsf 1'
+   eval $reset; dd if=$dev bs=32k of=out1
+   eval $reset; dd if=$dev bs=32k of=out2 skip=1
+   
+   the resulting files, out1 and out2, would compare equal.  */
+    
+    struct stat stats;
+    
+    return (fstat(fdesc, &stats) == 0
+            && (S_ISCHR(stats.st_mode)));
+}
+
+/* Throw away RECORDS blocks of BLOCKSIZE bytes on file descriptor FDESC,
+   which is open with read permission for FILE.  Store up to BLOCKSIZE
+   bytes of the data at a time in BUF, if necessary.  RECORDS must be
+   nonzero.  */
+
+void skip(int fdesc, char *file, uintmax_t records, size_t blocksize,
+                 unsigned char *buf)
+{
+    off_t offset = records * blocksize;
+    
+/* Try lseek and if an error indicates it was an inappropriate
+   operation, fall back on using read.  Some broken versions of
+   lseek may return zero, so count that as an error too as a valid
+   zero return is not possible here.  */
+    
+    if (offset / blocksize != records
+        || buggy_lseek_support(fdesc)
+        || lseek(fdesc, offset, SEEK_CUR) <= 0)
+    {
+        while (records--) {
+            ssize_t nread = safe_read(fdesc, buf, blocksize);
+            if (nread < 0) {
+                fprintf(stderr, "%s: reading %s", strerror(errno), file);
+                quit(1);
+            }
+            /* POSIX doesn't say what to do when dd detects it has been
+               asked to skip past EOF, so I assume it's non-fatal.
+               FIXME: maybe give a warning.  */
+            if (nread == 0)
+                break;
+        }
+    }
+}
+
+void time_left(char *secstr, size_t bufsize, int seconds)
+{
+    int hr = seconds / (60 * 60);
+    int min = seconds / 60 - hr * 60;
+    int sec = seconds - (hr * 60 * 60 + min * 60);
+
+    snprintf(secstr, bufsize, "%.02d:%.02d:%.02d remaining.", hr, min, sec);
+}
+
+/* Swap NREAD bytes in BUF, plus possibly an initial char from the
+   previous call.  If NREAD is odd, save the last char for the
+   next call.   Return the new start of the BUF buffer.  */
+
+unsigned char *swab_buffer(unsigned char *buf, size_t *nread)
+{
+    unsigned char *bufstart = buf;
+    register unsigned char *cp;
+    register int i;
+    
+/* Is a char left from last time?  */
+    if (char_is_saved) {
+        *--bufstart = saved_char;
+        (*nread)++;
+        char_is_saved = 0;
+    }
+    
+    if (*nread & 1) {
+        /* An odd number of chars are in the buffer.  */
+        saved_char = bufstart[--*nread];
+        char_is_saved = 1;
+    }
+    
+/* Do the byte-swapping by moving every second character two
+   positions toward the end, working from the end of the buffer
+   toward the beginning.  This way we only move half of the data.  */
+    
+    cp = bufstart + *nread;	/* Start one char past the last.  */
+    for (i = *nread / 2; i; i--, cp -= 2)
+        *cp = *(cp - 2);
+    
+    return ++bufstart;
+}
+
+/* Return the number of 1 bits in `i'. */
+
+int bit_count(register unsigned int i)
+{
+    register int set_bits;
+
+    for (set_bits = 0; i != 0; set_bits++)
+        i &= i - 1;
+    return set_bits;
+}
+
