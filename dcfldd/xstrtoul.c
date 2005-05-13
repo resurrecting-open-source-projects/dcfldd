fdcd7e497459d50314ed932b262c4c9221223d35
harbourn <harbourn@9a5663bd-6d3a-0410-9de0-8d6a123c63df>
2005-05-13 18:56:04 +0000

    added HAVE_DECL_STRTOUL wrapper

--- dcfldd/xstrtoul.c
+++ dcfldd/xstrtoul.c
@@ -1,4 +1,10 @@
+#if HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#if HAVE_DECL_STRTOUL
 #define __strtol strtoul
 #define __strtol_t unsigned long int
 #define __xstrtol xstrtoul
 #include "xstrtol.c"
+#endif
