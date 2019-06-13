44bae9278de09495d3cccfe6a5aa33701529bfb2
Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
2019-06-13 14:10:34 -0300

    Migrated manpage to txt2man system

---
+++ man/create-man.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Copyright 2015-2019 Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
+# Create a manpage using txt2man command.
+#
+# This script can be used under BSD-3-Clause license.
+
+T2M_DATE="12 June 2019"
+T2M_NAME=dcfldd
+T2M_VERSION=1.5
+T2M_LEVEL=1
+T2M_DESC="enhanced version of dd for forensics and security"
+
+# Don't change the following lines
+TEST=$(txt2man -h 2> /dev/null)
+[ "$TEST" ] || { echo -e "\nYou need to install txt2man, from https://github.com/mvertes/txt2man.\n"; exit 1; }
+
+txt2man -d "$T2M_DATE" -t $T2M_NAME -r $T2M_NAME-$T2M_VERSION -s $T2M_LEVEL -v "$T2M_DESC" $T2M_NAME.txt > $T2M_NAME.$T2M_LEVEL
