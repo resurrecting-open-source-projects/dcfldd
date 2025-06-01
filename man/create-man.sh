73dba40f7fbf90d57583c9028faa878390259397
David Polverari <david.polverari@gmail.com>
2025-06-01 21:14:12 +0000

    Update manpage

--- man/create-man.sh
+++ man/create-man.sh
@@ -16,9 +16,9 @@ txt2man -d "$T2M_DATE" -t $T2M_NAME -r $T2M_NAME-$T2M_VERSION -s $T2M_LEVEL -v "
 #--------------------------------------------------------
 
 # Put here all data for your first manpage (in T2M lines)
-T2M_DATE="20 Oct 2024"
+T2M_DATE="01 May 2025"
 T2M_NAME=dcfldd
-T2M_VERSION=1.9.2
+T2M_VERSION=1.9.3
 T2M_LEVEL=1
 T2M_DESC="enhanced version of dd for forensics and security"
 create-man
